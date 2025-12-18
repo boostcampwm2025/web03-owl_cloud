70f1fbf4096f3aa52509ad0f2fa85a4c1b65fb6e
지태 <wlxo402@naver.com>
2025-12-18 14:27:54 +0900

    📍 Feat: 워크스페이스 item rendering 구현

--- frontend/src/components/workspace/WorkspaceStage.tsx
+++ frontend/src/components/workspace/WorkspaceStage.tsx
@@ -1,34 +1,117 @@
 'use client';
 
-import React, { useRef } from 'react';
-import { Stage, Layer, Rect } from 'react-konva';
+import React, { useRef, useEffect, useState } from 'react';
+import { Stage, Layer, Rect, Transformer } from 'react-konva';
 import { useWorkspaceStore } from '@/store/useWorkspaceStore';
 
+import RenderItem from './items/RenderItem';
+
 export default function WorkspaceStage() {
-  const { cardData, zoom } = useWorkspaceStore();
+  // Store 상태 및 액션
+  const { cardData, selectedId, selectItem, updateItem, zoom } =
+    useWorkspaceStore();
+
+  // 접근 Ref 설정 (stage : 워크스페이스 / transformer : 선택 및 변형 도구)
   const stageRef = useRef<any>(null);
+  const transformerRef = useRef<any>(null);
+
+  // Hydration 방지용
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    // 추후에 수정이 필요하신경우 참고 부탁드립니다.
+    // ESLint에서 잘못 탐지 하는 부분(하단에 ESLint 주석 넣은 이유)
+    // useEffect에서 setState 호출하면 무한루프에 빠질 수도 있다고 경고하는부분인데
+    // 이 부분은 컴포넌트가 처음 마운트 될 때만 실행되므로 문제 없음(무한루프 발생하지 않음) : 정상 작동함
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    // mount 안되어 있거나 ref 연결 없으면 종료
+    if (mounted && transformerRef.current && stageRef.current) {
+      const stage = stageRef.current;
+
+      // 아이템 선택 확인
+      if (selectedId) {
+        // 아이템 노드 찾기
+        const selectedNode = stage.findOne('#' + selectedId);
+
+        // 찾으면 transformer node 배열 추가 후 화면 갱신
+        // 못찾으면 빈 배열
+        if (selectedNode) {
+          transformerRef.current.nodes([selectedNode]);
+          transformerRef.current.getLayer().batchDraw();
+        } else {
+          transformerRef.current.nodes([]);
+        }
+      } else {
+        // 선택된게 없으면 빈배열
+        transformerRef.current.nodes([]);
+      }
+    }
+  }, [selectedId, cardData.items, mounted]);
+
+  // 선택 해제
+  const handleCheckDeselect = (e: any) => {
+    // 선택 대상이 스테이지인 경우(하단 워크스페이스가 bg-rect)
+    const clickedOnEmpty =
+      e.target === e.target.getStage() || e.target.hasName('bg-rect');
+
+    // 선택 해제
+    if (clickedOnEmpty) {
+      selectItem(null);
+    }
+  };
+
+  if (!mounted) return null;
 
   return (
-    <div className="bg-white">
+    <div className="bg-white shadow-2xl">
       <Stage
         width={cardData.workspaceWidth * zoom}
         height={cardData.workspaceHeight * zoom}
         scaleX={zoom}
         scaleY={zoom}
+        onMouseDown={handleCheckDeselect}
+        onTouchStart={handleCheckDeselect}
         ref={stageRef}
         className="bg-white"
       >
         <Layer>
-          {/* 배경색 (캔버스 영역) */}
+          {/* 워크스페이스 */}
           <Rect
+            name="bg-rect"
             x={0}
             y={0}
             width={cardData.workspaceWidth}
             height={cardData.workspaceHeight}
             fill={cardData.backgroundColor}
-            shadowBlur={10}
+            // 클릭 이벤트 확인
+            listening={true}
           />
-          {/* TODO: <RenderItem />과 <Transformer />가 들어갈 예정 */}
+
+          {/* 아이템 렌더링 */}
+          {cardData.items.map((item) => (
+            <RenderItem
+              key={item.id}
+              item={item}
+              isSelected={item.id === selectedId}
+              onSelect={selectItem}
+              onChange={(newAttributes) => updateItem(item.id, newAttributes)}
+            />
+          ))}
+
+          {/* 선택,변형 */}
+          <Transformer
+            ref={transformerRef}
+            // 박스 크기 제한
+            boundBoxFunc={(oldBox, newBox) => {
+              if (newBox.width < 5 || newBox.height < 5) {
+                return oldBox;
+              }
+              return newBox;
+            }}
+          />
         </Layer>
       </Stage>
     </div>
---
+++ frontend/src/components/workspace/items/RenderItem.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React from 'react';
+import { WorkspaceItem } from '@/types/Workspace';
+
+// 아이템 컴포넌트
+// import TextItem from './TextItem';
+// import ImageItem from './ImageItem';
+// import VideoItem from './VideoItem';
+
+interface RenderItemProps {
+  item: WorkspaceItem;
+  isSelected: boolean;
+  onSelect: (id: string) => void;
+  onChange: (newAttributes: Partial<WorkspaceItem>) => void;
+}
+
+export default function RenderItem({
+  item,
+  isSelected,
+  onSelect,
+  onChange,
+}: RenderItemProps) {
+  switch (item.type) {
+    case 'text':
+      //   <TextItem
+      //     item={item}
+      //     isSelected={isSelected}
+      //     onSelect={() => onSelect(item.id)}
+      //     onChange={onChange}
+      //   />;
+      return null;
+
+    case 'image':
+      //   <ImageItem
+      //     item={item}
+      //     isSelected={isSelected}
+      //     onSelect={() => onSelect(item.id)}
+      //     onChange={onChange}
+      //   />;
+      return null;
+
+    case 'video':
+      //   <VideoItem
+      //     item={item}
+      //     isSelected={isSelected}
+      //     onSelect={() => onSelect(item.id)}
+      //     onChange={onChange}
+      //   />;
+      return null;
+
+    default:
+      return null;
+  }
+}
