fb4f3a78b80962907f8b67b77c0cfad63179bcd4
seorang42 <seorang42@naver.com>
2026-01-05 16:42:50 +0900

    ✂️ Remove: 프로젝트 파일 정리
    Workspace 관련 파일을 제외한 이전 프로젝트의 파일을 정리하였습니다.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import localFont from 'next/font/local';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 const suit = localFont({
   src: './fonts/SUIT-Variable.woff2',
@@ -30,8 +19,8 @@ const cafe24 = localFont({
 });
 
 export const metadata: Metadata = {
-  title: '클로비 - Clobby',
-  description: '취미, 취향으로 연결하고, 누군가의 시작이 되어보세요.',
+  title: 'DEVMEET',
+  description: '코드 에디터 화상통화',
 };
 
 export default function RootLayout({
