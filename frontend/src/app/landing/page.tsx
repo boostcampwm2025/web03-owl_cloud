2d5c2f5e550d79ec23034a06446aa200a881fe67
chamny20 <dge3179@ajou.ac.kr>
2026-01-05 22:19:23 +0900

    🎨 Style: 시멘틱 태그 적용 및 접근성 개선

--- frontend/src/app/landing/page.tsx
+++ frontend/src/app/landing/page.tsx
@@ -2,35 +2,53 @@ import Button from '@/components/common/button';
 
 export default function LandingPage() {
   return (
-    <main className="flex h-screen items-center justify-center">
-      <section className="m-auto flex w-full max-w-90 flex-col gap-16 px-6 py-4">
-        <div className="flex flex-col items-center justify-center gap-8">
-          <span className="text-[24px] font-bold text-neutral-900">
+    <main className="flex min-h-screen items-center justify-center">
+      <section
+        className="m-auto flex w-full max-w-90 flex-col gap-16 px-6 py-4"
+        aria-labelledby="start-meeting-title"
+      >
+        <header className="flex flex-col items-center gap-8">
+          <h1
+            id="start-meeting-title"
+            className="text-[24px] font-bold text-neutral-900"
+          >
             새 회의 시작
-          </span>
+          </h1>
           <Button>시작하기</Button>
+        </header>
+
+        {/* 구분선 */}
+        <div
+          className="flex w-full items-center gap-4 text-neutral-500"
+          aria-hidden
+        >
+          <span className="h-px flex-1 bg-neutral-500" />
+          <span className="text-[14px] font-bold">또는</span>
+          <span className="h-px flex-1 bg-neutral-500" />
         </div>
 
-        <div className="flex w-full items-center gap-4 text-neutral-500">
-          <span className="h-px flex-1 bg-neutral-500" />
-          <span className="text-[14px] font-bold text-neutral-500">또는</span>
-          <span className="h-px flex-1 bg-neutral-500" />
-        </div>
-
-        <div className="flex flex-col items-center justify-center gap-8">
-          <span className="text-[24px] font-bold text-neutral-900">
+        <section aria-labelledby="join-meeting-title">
+          <h2
+            id="join-meeting-title"
+            className="mb-8 text-center text-[24px] font-bold text-neutral-900"
+          >
             회의 참여하기
-          </span>
+          </h2>
 
-          <div className="flex w-full flex-col gap-6">
-            {/* TODO: input common-component 적용 */}
+          <form className="flex flex-col gap-6">
+            <label htmlFor="meeting-code" className="sr-only">
+              회의 코드 또는 링크
+            </label>
+
             <input
+              id="meeting-code"
               placeholder="코드 또는 링크를 입력해주세요"
-              className="h-11 w-full max-w-78 rounded-sm border border-neutral-300 px-2 py-3 outline-none"
+              className="focus:border-primary-sky1 h-11 w-full rounded-sm border border-neutral-300 px-3 outline-none"
             />
-            <Button>참여하기</Button>
-          </div>
-        </div>
+
+            <Button type="submit">참여하기</Button>
+          </form>
+        </section>
       </section>
     </main>
   );
