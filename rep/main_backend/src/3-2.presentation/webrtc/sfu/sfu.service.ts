3d8e98cdc02cb255c2da1750d53fcb2abe086122
KimDwDev <dongwan123456789@gmail.com>
2026-01-09 08:03:22 +0900

    🔨 Fix: 메모리를 repo로 사용

--- rep/main_backend/src/3-2.presentation/webrtc/sfu/sfu.service.ts
+++ rep/main_backend/src/3-2.presentation/webrtc/sfu/sfu.service.ts
@@ -9,6 +9,7 @@ import { CACHE_SFU_NAMESPACE_NAME } from "@infra/cache/cache.constants";
 import { SelectSfuTransportDataFromRedis } from "@infra/cache/redis/sfu/sfu.inbound";
 import { RoomTransportInfo } from "@app/sfu/queries/dto";
 import { CreateRouterUsecase } from "@app/sfu/commands/usecase";
+import { RoomRouterRepository } from "@infra/memory/sfu";
 
 
 @Injectable()
@@ -17,16 +18,15 @@ export class SfuService {
   // sfu 서버가 관리하는 로직 ( 메모리 낭비가 있는데 어떻게 하면 좀 효율적으로 저장이 가능할까? )
   private readonly logger = new Logger(SfuService.name);
   
-  // wokre들과 router 정리
-  private readonly roomRouters = new Map<string, RoomEntry>(); // room_id : RoomEntry
-
   // transport가 저장이 되야 한다. 
   private readonly transports = new Map<string, WebRtcTransport>(); // transport_id : transport 저장 ( 나중에 transport를  )
 
   constructor(
     // usecase
     private readonly createRouterUsecase : CreateRouterUsecase,
-    // 
+    // infra
+    private readonly roomRouters : RoomRouterRepository,
+
     private readonly insertTranportInfoToRedis : CreateSfuTransportInfoToRedis,
     private readonly deleteTransportInfoToRedis : DeleteSfuTransportInfoToRedis,
     private readonly selectSfuTransportInfoFromRedis : SelectSfuTransportDataFromRedis
