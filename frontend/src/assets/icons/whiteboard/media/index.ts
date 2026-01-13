3f4c4ac77cdeb1f5b1f09f50869838d6a5f38eeb
지태 <jitae@jitaeui-MacBookPro.local>
2026-01-14 02:00:58 +0900

    📍 Feat: mute,unmute svg 아이콘 추가

--- frontend/src/assets/icons/whiteboard/media/index.ts
+++ frontend/src/assets/icons/whiteboard/media/index.ts
@@ -2,3 +2,5 @@
 export { default as ImageIcon } from '@/assets/icons/whiteboard/media/imageIcon.svg';
 export { default as VideoIcon } from '@/assets/icons/whiteboard/media/videoIcon.svg';
 export { default as YoutubeIcon } from '@/assets/icons/whiteboard/media/youtubeIcon.svg';
+export { default as MuteIcon } from '@/assets/icons/whiteboard/media/mute.svg';
+export { default as UnmuteIcon } from '@/assets/icons/whiteboard/media/unmute.svg';
