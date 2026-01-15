f8b617a7625acabbf830c9b1b015a85627d95938
KimDwDev <dongwan123456789@gmail.com>
2026-01-15 19:24:52 +0900

    🚚 Chore: main_backend + kafka 연결

---
+++ rep/main_backend/src/3-1.infra/event-stream/event-stream.constants.ts
@@ -0,0 +1,2 @@
+
+export const KAFKA = Symbol("KAFKA");
--- rep/main_backend/src/main.ts
+++ rep/main_backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { RedisIoAdapter } from '@infra/channel/redis/channel.service';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { SASLOptions } from 'kafkajs';
 
 async function bootstrap() {
   // 기본 설정
@@ -12,6 +14,39 @@ async function bootstrap() {
   const redisAdapter = new RedisIoAdapter(app, config);
   await redisAdapter.websocketConnectToRedis(); // websocket을 redis로 연결
   app.useWebSocketAdapter(redisAdapter);
+
+  // kafka 설정 - consumers 설정
+  const clientId : string = config.get<string>('NODE_APP_KAFKA_CLIENT_ID', "client_id")
+  const brokers : Array<string> = config.get<string>('NODE_APP_KAFKA_BROKERS', "127.0.0.1:9092")
+  .split(',')
+  .map(v => v.trim());
+  const groupId = config.get<string>('NODE_APP_KAFKA_GROUP_ID', "groupid");
+
+  // sasl 
+  const saslMechanism = config.get<string>('NODE_APP_KAFKA_SASL_MECHANISM', "scram-sha-256") as "scram-sha-256"; // 가장 보안적으로 않좋다. 
+  // ssl에 쓰이는 이름
+  const username : string = config.get<string>('NODE_APP_KAFKA_SASL_USERNAME', "main-user")
+  const password : string = config.get<string>('NODE_APP_KAFKA_SASL_PASSWORD', "password")
+  // ssl 사용
+  const sslEnabled : boolean = config.get<string>('NODE_APP_KAFKA_SSL_ENABLED', "false") === 'true';
+
+  // 구독한 topic에서 받는 것에 대한 설정
+  const sasl: SASLOptions = { mechanism: saslMechanism, username, password };
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId,
+        brokers,
+        ssl: sslEnabled,
+        ...(sasl ? { sasl } : {}),
+      },
+      consumer: { groupId },
+      subscribe: { fromBeginning: false }, // 현재 시점 이후에만 읽겠다는 뜻이다. 
+    },
+  });
+  await app.startAllMicroservices(); // 실제 실행 
 
   // cors 설정
   const origin : Array<string> = config
