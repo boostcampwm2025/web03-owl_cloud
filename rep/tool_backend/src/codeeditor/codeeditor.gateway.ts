395b2c60aa66e3621a159730ec6c1d469dfa3e97
seorang42 <seorang42@naver.com>
2026-01-20 22:13:17 +0900

    🔨Fix: 백엔드 변경사항 누락 포함

--- rep/tool_backend/src/codeeditor/codeeditor.gateway.ts
+++ rep/tool_backend/src/codeeditor/codeeditor.gateway.ts
@@ -2,6 +2,7 @@ import { AuthType, ToolBackendPayload } from '@/guards/guard.type';
 import { Inject, Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayInit,
   SubscribeMessage,
@@ -17,7 +18,16 @@ import { EVENT_STREAM_NAME } from '@/infra/event-stream/event-stream.constants';
 import { CODEEDITOR_WEBSOCKET } from '@/infra/websocket/websocket.constants';
 import { CodeeditorWebsocket } from '@/infra/websocket/codeeditor/codeeditor.service';
 
-// 아래쪽에 whiteboard 관련 @Submessage를 붙이셔서 해주시면 될것 같아요 ㅎㅎ
+const mockData: ToolBackendPayload = {
+  user_id: 'user1',
+  room_id: 'user2',
+  tool: 'codeeditor',
+  socket_id: 'socket1',
+  ticket: 'ticket',
+  clientType: 'main',
+  // nickname: 'test_user',
+};
+
 @WebSocketGateway({
   namespace: process.env.NODE_BACKEND_WEBSOCKET_PREFIX,
   path: process.env.NODE_BACKEND_WEBSOCKET_CODEEDITOR, // http 핸드세이킹이 있을때 붙게 되는
@@ -36,7 +46,7 @@ export class CodeeditorWebsocketGateway implements OnGatewayInit, OnGatewayConne
 
   constructor(
     private readonly codeeditorService: CodeeditorService,
-    private readonly kafkaService: KafkaService,
+    // private readonly kafkaService: KafkaService,
     @Inject(CODEEDITOR_WEBSOCKET) private readonly codeeditorSocket: CodeeditorWebsocket,
   ) {}
 
@@ -45,22 +55,25 @@ export class CodeeditorWebsocketGateway implements OnGatewayInit, OnGatewayConne
     this.codeeditorSocket.bindServer(server);
 
     server.use(async (socket, next) => {
-      try {
-        const { token, type } = socket.handshake.auth as AuthType;
+      socket.data.payload = mockData;
+      this.logger.log('웹소켓 준비되었습니다.');
+      return next();
+      // try {
+      //   const { token, type } = socket.handshake.auth as AuthType;
 
-        if (!token) return next(new Error('TOKEN_REQUIRED'));
-        if (type !== 'main' && type !== 'sub') return next(new Error('INVALID_TYPE'));
+      //   if (!token) return next(new Error('TOKEN_REQUIRED'));
+      //   if (type !== 'main' && type !== 'sub') return next(new Error('INVALID_TYPE'));
 
-        const payload = await this.codeeditorService.guardService(token, type);
+      //   const payload = await this.whiteboarService.guardService(token, type);
 
-        // data 추가
-        socket.data.payload = payload;
-        this.logger.log('웹소켓 준비되었습니다.');
-        return next();
-      } catch (err) {
-        this.logger.error(err);
-        next(new Error('인증 에러'));
-      }
+      //   // data 추가
+      //   socket.data.payload = payload;
+      //   this.logger.log('웹소켓 준비되었습니다.');
+      //   return next();
+      // } catch (err) {
+      //   this.logger.error(err);
+      //   next(new Error('인증 에러'));
+      // }
     });
   }
 
@@ -72,20 +85,28 @@ export class CodeeditorWebsocketGateway implements OnGatewayInit, OnGatewayConne
       return;
     }
 
-    // 방 입장후 알림
-    const namespace: string = this.codeeditorService.makeNamespace(payload.room_id); // 방가입
-    client.join(namespace);
+    const roomName = this.codeeditorService.makeNamespace(payload.room_id); // 방가입
+    client.join(roomName);
 
-    if (payload.clientType === 'main') {
-      this.kafkaService.emit(EVENT_STREAM_NAME.CODEEDITOR_ENTER, {
-        room_id: payload.room_id,
-        user_id: payload.user_id,
-        tool: payload.tool,
-        socket_id: payload.socket_id,
-        ticket: payload.ticket,
-        at: Date.now(), // 현재 보낸 시간
-      });
-    }
+    client.data.roomName = roomName;
+
+    client.emit('init-user', {
+      userId: payload.user_id,
+    });
+
+    // 방에 이미 사람이 있다면, 그중 한 명에게 최신 상태(Sync Step)를 요청
+    client.to(roomName).emit('request-sync');
+
+    // if (payload.clientType === 'main') {
+    //   this.kafkaService.emit(EVENT_STREAM_NAME.CODEEDITOR_ENTER, {
+    //     room_id: payload.room_id,
+    //     user_id: payload.user_id,
+    //     tool: payload.tool,
+    //     socket_id: payload.socket_id,
+    //     ticket: payload.ticket,
+    //     at: Date.now(), // 현재 보낸 시간
+    //   });
+    // }
 
     client.emit(CODEEDITOR_CLIENT_EVENT_NAME.PERMISSION, { ok: true });
   }
@@ -103,6 +124,36 @@ export class CodeeditorWebsocketGateway implements OnGatewayInit, OnGatewayConne
     } catch (err) {
       this.logger.error(err);
       throw new WsException({ message: err.message ?? '에러 발생', status: err.status ?? 500 });
+    }
+  }
+
+  @SubscribeMessage('yjs-update')
+  handleYjsUpdate(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() update: Buffer, // Yjs 데이터는 바이너리
+  ) {
+    try {
+      if (!update || update.length === 0) return;
+
+      // 캐싱된 룸 이름 사용
+      const roomName = client.data.roomName;
+
+      // 브로드캐스트
+      // volatile을 붙이면 전송 실패 시 재시도하지 않아 실시간 성능에 유리
+      client.to(roomName).volatile.emit('yjs-update', update);
+    } catch (error) {
+      this.logger.error(`Yjs Update Error: ${error.message}`);
+    }
+  }
+
+  @SubscribeMessage('awareness-update')
+  handleAwarenessUpdate(@ConnectedSocket() client: Socket, @MessageBody() update: Buffer) {
+    try {
+      if (!update) return;
+
+      client.to(client.data.roomName).volatile.emit('awareness-update', update);
+    } catch (error) {
+      this.logger.error(`Awareness Update Error: ${error.message}`);
     }
   }
 }
