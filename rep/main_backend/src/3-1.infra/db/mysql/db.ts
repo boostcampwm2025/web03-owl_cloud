e9b6eb52f913df597a29cfefd7a5a80f62e9e4e0
KimDwDev <dongwan123456789@gmail.com>
2026-01-28 08:32:38 +0900

    📍 Feat: 유저의 정보와 일치하는 방의 정보를 가져오는 로직 구현

--- rep/main_backend/src/3-1.infra/db/mysql/db.ts
+++ rep/main_backend/src/3-1.infra/db/mysql/db.ts
@@ -15,7 +15,7 @@ import {
   InsertRoomParticipantInfoDataToMysql,
   UpdateRoomParticipantInfoToMysql,
 } from './room/room.outbound';
-import { SelectRoomDataFromMysql, SelectRoomIdFromMysql } from './room/room.inbound';
+import { SelectRoomDataFromMysql, SelectRoomIdFromMysql, SelectUserInfoRoomFromMysql } from './room/room.inbound';
 
 @Global()
 @Module({
@@ -58,6 +58,7 @@ import { SelectRoomDataFromMysql, SelectRoomIdFromMysql } from './room/room.inbo
     UpdateRoomParticipantInfoToMysql, // 회의방에 참가자 떠났다는 정보 기입
     SelectUserAndOauthFromMysql, // oauth에서 유저의 정보를 찾아주는
     SelectRoomIdFromMysql, // room_id를 찾아주는
+    SelectUserInfoRoomFromMysql, // user_id에 따른 방에 정보를 가져오는 로직 
   ],
   exports: [
     MYSQL_DB,
@@ -73,6 +74,7 @@ import { SelectRoomDataFromMysql, SelectRoomIdFromMysql } from './room/room.inbo
     UpdateRoomParticipantInfoToMysql,
     SelectUserAndOauthFromMysql,
     SelectRoomIdFromMysql,
+    SelectUserInfoRoomFromMysql,
   ],
 })
 export class MysqlModule {}
